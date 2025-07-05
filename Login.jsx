92e0661b8c331938ed3756874cbfda7214427f01
Jayesh-Jain31 <jayeshjain66192@gmail.com>
2025-07-06 01:32:45 +0530

    Add files via upload

---
+++ Login.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const Login = () => <p>Login form goes here</p>;
+export default Login;
---
+++ Signup.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const Signup = () => <p>Signup form goes here</p>;
+export default Signup;
